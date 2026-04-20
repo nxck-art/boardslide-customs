f65a6e38575ef58a6af928322eba620498b57624
nxck-art <m4shermangeek@yahoo.com>
2026-04-20 14:41:06 -0400

    The Great Lock-in of April 20th, 2026.

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect
 from models import db
 import models
 from models.product import Product
+from models.customer import Customer
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -23,7 +24,7 @@ def addproduct():
     if request.method == 'POST':
         product = Product(
             name = request.form['name'],
-            cat = request.form['category'],
+            category = request.form['category'],
             price = float(request.form['price']),
             quantity = int(request.form['quantity'])
         )
@@ -32,6 +33,49 @@ def addproduct():
         return redirect('/products')
     
     return render_template('addproduct.html')
+
+@app.route('/edit_product/<int:id>', methods=['GET', 'POST'])
+def updateproduct():
+    product = Product.query.get(id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.category = request.form['category']
+        product.price = float(request.form['price'])
+        product.quantity = int(request.form['quantity'])
+
+        db.session.commit()
+        return redirect('/products')
+    return render_template('editproduct.html', product=product)
+
+@app.route('/delete_product/<int:id>')
+def deleteproduct():
+    product = Product.query.get(id)
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return redirect('/products')
+
+#customer management
+@app.route('/customers')
+def viewall():
+    customers = Customer.query.all()
+    return render_template('customers.html', customers = customers)
+
+@app.route('/add_customer', methods=['GET', 'POST'])
+def addcustomer():
+    if request.method == "POST":
+        customer = Customer(
+            fname = request.form['fname'],
+            lname = request.form['lname'],
+            email = request.form['email'],
+            phone = request.form['phone'])
+
+        db.session.add(customer)
+        db.session.commit()
+
+        return redirect('/customers')
+    return render_template('addcustomer.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
