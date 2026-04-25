8d3c830c21e1db0fb744459268fb60b1f9b8bca4
nxck-art <m4shermangeek@yahoo.com>
2026-04-25 01:23:48 -0400

    Made changes to templates and updated backend.

--- application.py
+++ application.py
@@ -12,31 +12,38 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-#routes
 
-#HOME PAGE
 @app.route('/')
 def homepage():
-    return "<h1>Welcome to our most dope website!</h1>"
+    return render_template("index.html")
+
 
 @app.route('/add_product', methods=['GET', 'POST'])
 def addproduct():
     if request.method == 'POST':
         product = Product(
-            name = request.form['name'],
-            category = request.form['category'],
-            price = float(request.form['price']),
-            quantity = int(request.form['quantity'])
+            name=request.form['name'],
+            category=request.form['category'],
+            price=float(request.form['price']),
+            quantity=int(request.form['quantity'])
         )
         db.session.add(product)
         db.session.commit()
         return redirect('/products')
-    
+
     return render_template('addproduct.html')
 
+
+@app.route('/products')
+def view_products():
+    products = Product.query.all()
+    return render_template('products.html', products=products)
+
+
 @app.route('/edit_product/<int:id>', methods=['GET', 'POST'])
-def updateproduct():
+def updateproduct(id):
     product = Product.query.get(id)
+
     if request.method == 'POST':
         product.name = request.form['name']
         product.category = request.form['category']
@@ -45,10 +52,12 @@ def updateproduct():
 
         db.session.commit()
         return redirect('/products')
+
     return render_template('editproduct.html', product=product)
 
+
 @app.route('/delete_product/<int:id>')
-def deleteproduct():
+def deleteproduct(id):
     product = Product.query.get(id)
 
     db.session.delete(product)
@@ -56,26 +65,56 @@ def deleteproduct():
 
     return redirect('/products')
 
-#customer management
+
 @app.route('/customers')
 def viewall():
     customers = Customer.query.all()
-    return render_template('customers.html', customers = customers)
+    return render_template('customers.html', customers=customers)
+
 
 @app.route('/add_customer', methods=['GET', 'POST'])
 def addcustomer():
     if request.method == "POST":
         customer = Customer(
-            fname = request.form['fname'],
-            lname = request.form['lname'],
-            email = request.form['email'],
-            phone = request.form['phone'])
+            fname=request.form['fname'],
+            lname=request.form['lname'],
+            email=request.form['email'],
+            phone=request.form['phone']
+        )
 
         db.session.add(customer)
         db.session.commit()
 
         return redirect('/customers')
+
     return render_template('addcustomer.html')
+
+
+@app.route('/edit_customer/<int:id>', methods=['GET', 'POST'])
+def edit_customer(id):
+    customer = Customer.query.get(id)
+
+    if request.method == 'POST':
+        customer.fname = request.form['fname']
+        customer.lname = request.form['lname']
+        customer.email = request.form['email']
+        customer.phone = request.form['phone']
+
+        db.session.commit()
+        return redirect('/customers')
+
+    return render_template('editcustomer.html', customer=customer)
+
+
+@app.route('/delete_customer/<int:id>')
+def delete_customer(id):
+    customer = Customer.query.get(id)
+
+    db.session.delete(customer)
+    db.session.commit()
+
+    return redirect('/customers')
+
 
 if __name__ == '__main__':
     app.run(debug=False)
